a19d035ee525648f4da000003f97157040092a49
Aryan <aryanbisht9458@gmail.com>
2021-03-22 22:32:55 +0530

    thrid commit
    Added new Activity of Step3

--- app/src/main/java/com/example/shamointern/Step2FirstActivity.java
+++ app/src/main/java/com/example/shamointern/Step2FirstActivity.java
@@ -2,14 +2,28 @@ package com.example.shamointern;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.shamointern.databinding.ActivityStep2FirstBinding;
 
 public class Step2FirstActivity extends AppCompatActivity {
 
+    ActivityStep2FirstBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_step2_first);
+        binding = ActivityStep2FirstBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         getSupportActionBar().hide();
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Step2FirstActivity.this,Step2SecondActivity.class));
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/shamointern/Step3FirstActivity.java
@@ -0,0 +1,14 @@
+package com.example.shamointern;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Step3FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step3_first);
+    }
+}
